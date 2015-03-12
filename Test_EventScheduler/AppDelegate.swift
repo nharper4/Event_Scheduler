6aff30a6550ac0b4346a840ee586dfabf757329a
Nae Nae <nate72692@nathan-harpers-MacBook-Pro.local>
2015-03-12 18:12:32 -0400

    Initial Commit

---
+++ Test_EventScheduler/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Test_EventScheduler
+//
+//  Created by Nae Nae on 3/12/15.
+//  Copyright (c) 2015 OneOfOne. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
